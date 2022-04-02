8bae9c2c48ee11eff474023aa0d883f4b6c3dbc0
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-04-02 15:56:22 -0300

    feat: user data update

--- backend/src/controllers/EditUserController.js
+++ backend/src/controllers/EditUserController.js
@@ -1,7 +1,73 @@
+const User = require("../models/User");
+const getUserByToken = require("../helpers/GetUserByToken");
+const getToken = require("../helpers/GetToken");
+const bcrypt = require("bcrypt");
+
 class EditUserController {
   static async handle(req, res) {
-    res.status(200).json({ message: "Deu certo" });
-    return;
+    const id = req.params.id;
+
+    // get user by token
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    let image = "";
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "Name is required" });
+      return;
+    }
+
+    user.name = name;
+
+    if (!email) {
+      res.status(422).json({ message: "E-mail is required" });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (user.email !== email && userExists) {
+      return res.status(401).json({ message: "User already exists" });
+    }
+
+    user.email = email;
+
+    if (!phone) {
+      res.status(422).json({ message: "Phone is required" });
+      return;
+    }
+
+    user.phone = phone;
+
+    if (password !== confirmpassword) {
+      return res
+        .status(422)
+        .json({ message: "Password and confirmation must match" });
+    } else if (password === confirmpassword && password !== null) {
+      // create password
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      user.password = passwordHash;
+    }
+
+    try {
+      // return user update data
+      await User.findOneAndUpdate(
+        { _id: user._id },
+        { $set: user },
+        { new: true }
+      );
+
+      return res.status(200).json({ message: "User updated successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
   }
 }
 
---
+++ backend/src/helpers/GetUserByToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const auth = require("../config/auth");
+const User = require("../models/User");
+
+const GetUserByToken = async (token) => {
+  if (!token) {
+    return res.status(401).json({ message: "JWT token is missing" });
+  }
+
+  const decoded = jwt.verify(token, auth.jwt.secret);
+
+  const user_id = decoded.id;
+
+  const user = await User.findOne({ _id: user_id });
+
+  return user;
+};
+
+module.exports = GetUserByToken;
