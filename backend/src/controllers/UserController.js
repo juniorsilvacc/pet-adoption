97588f784d832d308b5fa43956ab91948b6efd91
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-04-02 03:01:00 -0300

    feat: system login function

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -67,4 +67,36 @@ module.exports = class UserController {
       res.status(500).json({ message: error });
     }
   }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "E-mail is required" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "Password is required" });
+      return;
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res.status(422).json({ message: "User not exists" });
+      return;
+    }
+
+    // check if password match with password
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: "Invalid email or password" });
+      return;
+    }
+
+    await createUserToken(user, req, res);
+  }
 };
