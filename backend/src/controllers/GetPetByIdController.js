8294a9e89d674e90b8adc6af0e04b734ce6d2a55
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-04-04 19:32:23 -0300

    feat: validating pets searches by id

--- backend/src/controllers/GetPetByIdController.js
+++ backend/src/controllers/GetPetByIdController.js
@@ -1,10 +1,19 @@
 const Pet = require("../models/Pet");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 class GetPetByIdController {
   static async handle(req, res) {
     const { id } = req.params;
 
+    if (!ObjectId.isValid(id)) {
+      return res.status(422).json({ message: "ID Invalid" });
+    }
+
     const pets = await Pet.findById(id);
+
+    if (!pets) {
+      res.status(404).json({ message: "Pet not found" });
+    }
 
     res.status(200).json({ pets });
   }
