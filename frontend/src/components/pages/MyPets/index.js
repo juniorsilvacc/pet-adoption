cb52639c6fbdbb8109f4a353bdc9fe1924c0e3af
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-04-14 18:51:39 -0300

    feat: get registered pets

--- frontend/src/components/pages/MyPets/index.js
+++ frontend/src/components/pages/MyPets/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import { useState, UseEffect } from "react";
-import Button from "../../form/Button";
+import { useState, useEffect } from "react";
 
 import { Div, Title, DivStyled, Text } from "./styles";
 
+// hooks
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
+// components
+import Button from "../../form/Button";
+
+import api from "../../../utils/api";
+
 export default function MyPets() {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets);
+      });
+  }, [token]);
 
   return (
     <Div>
@@ -17,8 +37,8 @@ export default function MyPets() {
       </Link>
 
       <DivStyled>
-        {pets.length > 0 && <Text>Meus Pets Cadastrados</Text>}
         {pets.length === 0 && <Text>Nenhum Pet Encontrado</Text>}
+        {pets.length > 0 && <Text>Meus Pets Cadastrados</Text>}
       </DivStyled>
     </Div>
   );
