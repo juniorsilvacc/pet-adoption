3c69da30d7cd1c5637d0726c273a9bfddf53a3ab
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-04-14 18:26:16 -0300

    feat: registering pet

--- frontend/src/components/pages/CreatePet/index.js
+++ frontend/src/components/pages/CreatePet/index.js
@@ -1,15 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Div, Title } from "./styles";
+
+// components
 import FormPet from "../../form/FormPet";
 
+// hooks
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
+import api from "../../../utils/api";
+
 export default function CreatePet() {
-  async function registerPet(pet) {}
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+  const history = useNavigate();
+
+  async function registerPet(pet) {
+    let messageType = "success";
+
+    const formData = new FormData();
+
+    await Object.keys(pet).forEach((key) => {
+      if (key === "images") {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append("images", pet[key][i]);
+        }
+      } else {
+        formData.append(key, pet[key]);
+      }
+    });
+
+    const data = await api
+      .post("/pets/create", formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        messageType = "error";
+        return err.response.data;
+      });
+
+    setFlashMessage(data.message, messageType);
+    if (messageType !== "error") {
+      history("/users/mypets");
+    }
+  }
 
   return (
     <Div>
       <Title>Cadastrar Pet</Title>
-
       <FormPet handleSubmit={registerPet} />
     </Div>
   );
